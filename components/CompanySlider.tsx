227946cebd7589d51ecd55b2317c4e0ee37bb703
samuelklepper <sklepper@bouncingpixel.com>
2024-03-02 18:12:17 -0600

    Company slider component updated to map through a companyLogos array and updated with latest projects logos.

--- components/CompanySlider.tsx
+++ components/CompanySlider.tsx
@@ -3,56 +3,89 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 export default function CompanySlider() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 4000,
-    autoplaySpeed: 4000,
-    cssEase: 'linear',
-  }
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 4000,
+        autoplaySpeed: 4000,
+        cssEase: 'linear',
+    }
 
-  return (
-    <div className='overflow-hidden'>
-      <Slider {...settings}>
-        <div className='col-span-1 flex justify-center items-center md:col-span-2 lg:col-span-1'>
-          <img
-            className='h-12 md:h-16 mx-auto'
-            src='/images/logos/wig-logo.png'
-            alt='WealthGuard'
-          />
+    const companyLogos = [
+        {
+            id: 1,
+            src: '/images/logos/winfields-logo.svg',
+            alt: 'Winfields Chocolate Bar',
+        },
+        {
+            id: 2,
+            src: '/images/logos/becks-logo.png',
+            alt: 'Becks Prime',
+        },
+        {
+            id: 3,
+            src: '/images/logos/cBit-logo.png',
+            alt: 'C-Bit Trainer',
+        },
+        {
+            id: 4,
+            src: '/images/logos/ticHelper-logo.png',
+            alt: 'TicHelper',
+        },
+        {
+            id: 5,
+            src: '/images/logos/SuperChef-logo.png',
+            alt: 'SuperChef',
+        },
+        {
+            id: 6,
+            src: '/images/logos/fha-logo.png',
+            alt: 'FHA',
+        },
+        {
+            id: 7,
+            src: '/images/logos/wig-logo.png',
+            alt: 'WealthGuard',
+        },
+        {
+            id: 8,
+            src: '/images/logos/edge196.svg',
+            alt: 'Edge196',
+        },
+        {
+            id: 9,
+            src: '/images/logos/dss.jpg',
+            alt: 'DeLeon Safety',
+        },
+        {
+            id: 10,
+            src: '/images/logos/ud.png',
+            alt: 'Ultra Demo',
+        },
+        {
+            id: 11,
+            src: '/images/logos/mHawk.png',
+            alt: 'Melissa Hawkins',
+        },
+    ]
+
+    return (
+        <div className='overflow-hidden'>
+            <Slider {...settings}>
+                {companyLogos.map((company) => (
+                    <div key={company.id}
+                         className='col-span-1 flex justify-center items-center md:col-span-2 lg:col-span-1'>
+                        <img
+                            className='h-12 md:h-16 mx-auto'
+                            src={company.src}
+                            alt={company.alt}
+                        />
+                    </div>
+                ))}
+            </Slider>
         </div>
-        <div className='col-span-1 flex justify-center items-center md:col-span-2 lg:col-span-1'>
-          <img
-            className='h-12 md:h-16 mx-auto'
-            src='/images/logos/edge196.svg'
-            alt='Edge196'
-          />
-        </div>
-        <div className='col-span-1 flex justify-center items-center md:col-span-2 lg:col-span-1'>
-          <img
-            className='h-12 md:h-16 mx-auto'
-            src='/images/logos/dss.jpg'
-            alt='DeLeon Safety'
-          />
-        </div>
-        <div className='col-span-1 flex justify-center items-center md:col-span-2 md:col-start-2 lg:col-span-1'>
-          <img
-            className='h-12 md:h-16 md:w-28 mx-auto'
-            src='/images/logos/ud.png'
-            alt='Ultra Demo'
-          />
-        </div>
-        <div className='col-span-2 flex justify-center items-center md:col-span-2 md:col-start-4 lg:col-span-1'>
-          <img
-            className='h-12 md:h-16 mx-auto'
-            src='/images/logos/mHawk.png'
-            alt='Melissa Hawkins'
-          />
-        </div>
-      </Slider>
-    </div>
-  )
+    )
 }
